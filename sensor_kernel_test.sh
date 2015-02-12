a360d6934e19baedcd7f73821428af8e377fbb48
Frank <frank770504@gmail.com>
2015-02-12 12:03:55 +0800

    read i2c bus

--- sensor_kernel_test.sh
+++ sensor_kernel_test.sh
@@ -14,6 +14,7 @@ _regulator="8226_l19 \
 
 checkc_regulator()
 {
+	echo "$0 ----------"
 	for _reg in $_regulator
 	do
 		echo "check regulator: $_reg"
@@ -26,26 +27,47 @@ checkc_regulator()
 	done
 }
 
-PUSH_PATH="system/bin"
-BIN_NAME=$1
+_PUSH_PATH="system/bin"
 
 #push binary
 push_bin()
 {
 #$1 the binary name
-#$2 the push path
+#$2 binary path
+#$3 the push path
 
-        adb push ./$1 $2
-        adb shell "chmod 755 $2/$1"
+        adb push $2/$1 $3
+        adb shell "chmod 755 $3/$1"
 }
 
 
-check_sensors_addr_on_bus()
+push_i2c_check_binary()
 {
-
+	echo "$0 ----------"
+	push_bin "i2c-util" "." $_PUSH_PATH
+	push_bin "i2cdetect" "i2ctool" "$_PUSH_PATH"
+	push_bin "i2cdump" "i2ctool" "$_PUSH_PATH"
+	push_bin "i2cget" "i2ctool" "$_PUSH_PATH"
+	push_bin "i2cset" "i2ctool" "$_PUSH_PATH"
 }
+
+check_sensors_on_bus()
+{
+	echo "$0 ----------"
+#$1 is the i2c bus bunber in the platform
+#make SENtral get into passthrough mode first
+	adb shell "rmmod em718x" #TODO what if SENtral init is failled
+	adb shell "insmod system/lib/modules/em718x.ko SENstr=\"passthrough\""
+	echo "all the device on i2c-$1"
+	adb shell "i2cdetect -y -r $1"
+	#TODO do other things like get sensors data direct from i2c
+	adb shell "rmmod em718x"
+	adb shell "insmod system/lib/modules/em718x.ko"
+}
+
 
 device_root
-echo "\n"
+echo ""
 checkc_regulator
-
+push_i2c_check_binary
+check_sensors_on_bus 2
